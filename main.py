b7c097e10c510ab5363cbf32f60f4dd0931b12ee
Stoica <stoicaastefan@gmail.com>
2021-06-16 09:51:59 +0300

    evaluation refactor

--- main.py
+++ main.py
@@ -33,48 +33,6 @@ if(args.evaluate is True):
     validationPipelines = ValidationPipelines()
 
     validationPipelines.KClusteringPipeline()
-    #validationPipelines.EstimatorPipeline()
-    #validationPipelines.SemisupervisedVotingPipeline()
-    #validationPipelines.SemiSupervisedMultiviewSpectralClustering()
-    '''
-    clusteringValidationMethod = ClusteringValidationMethod()
-    kmeans = KMeans(n_clusters = -1)
-    spectralClustering = SpectralClustering(n_clusters= -1)
-
-    clusteringValidationMethod.validateK(SafeEmbeddingsLoader(), [kmeans, spectralClustering])
-    clusteringValidationMethod.validateK(W2VEmbeddingsLoader(), [kmeans, spectralClustering])
-    clusteringValidationMethod.validateK(TfidfEmbeddingsLoader(), [kmeans, spectralClustering])
-
-    estimatorValidation = EstimatorValidationMethod()
-
-    estimatorValidation.validate(SafeEmbeddingsLoader(), [RandomForestClassifier(), SVC(), XGBClassifier(verbosity = 0)], 0.2)
-    estimatorValidation.validate(W2VEmbeddingsLoader(), [RandomForestClassifier(), SVC(), XGBClassifier(verbosity = 0)], 0.2)
-    estimatorValidation.validate(TfidfEmbeddingsLoader(), [RandomForestClassifier(), SVC(), XGBClassifier(verbosity = 0)], 0.2)
-
-
-    clusteringValidationMethod = ClusteringValidationMethod()
-    clusteringValidationMethod.validateSemiSupervised([W2VEmbeddingsLoader(), TfidfEmbeddingsLoader(), SafeEmbeddingsLoader()], [kmeans, kmeans, kmeans], [XGBClassifier(verbosity = 0), SVC(), RandomForestClassifier()])
-
-
-    clusteringValidationMethod = ClusteringValidationMethod()
-    clusteringValidationMethod.validateClusteringMultiView([W2VEmbeddingsLoader(), TfidfEmbeddingsLoader(),  SafeEmbeddingsLoader()])
-    '''
-'''
-predictionMethods = PredictionMethods()
-k = 2
-problem ="cmmdc"
-indices, Y = predictionMethods.validateSemiSupervised(problem, [W2VEmbeddingsLoader(), TfidfEmbeddingsLoader()], [KMeans(), SpectralClustering()], [XGBClassifier(verbosity = 0), SVC(kernel='rbf')], k)
-
-problemPath = f"Data/RawDataset/{problem}"
-
-if(os.path.exists(problemPath + "/clusters")):
-    shutil.rmtree(problemPath + "/clusters")
-
-os.mkdir(problemPath + "/clusters")
-
-for i in range(k):
-    os.mkdir(problemPath + "/clusters/" + str(i))
-
-for i in range(len(indices)):
-    shutil.copyfile(problemPath + '/100/' + f'{indices[i]}.cpp', problemPath + "/clusters/" + f'{Y[i]}/{indices[i]}.cpp')
-'''
+    validationPipelines.EstimatorPipeline()
+    validationPipelines.SemisupervisedVotingPipeline()
+    validationPipelines.SemiSupervisedMultiviewSpectralClustering()
