0a5c6669934a92d77d63c4ae314099c041ce2622
Stoica <stoicaastefan@gmail.com>
2021-06-28 01:44:01 +0300

    Modified main

--- main.py
+++ main.py
@@ -36,3 +36,27 @@ if(args.evaluate is True):
     validationPipelines.EstimatorPipeline()
     validationPipelines.SemisupervisedVotingPipeline()
     validationPipelines.SemiSupervisedMultiviewSpectralClustering()
+
+
+datasetStatistics = open("datasetStatistics.csv", "w")
+
+for root, folders, files in os.walk("Data/RawDataset"):
+    if(len(folders)==0):
+        problem = root.split("/")[-2]
+        solution = root.split("/")[-1]
+        
+        numberFiles = len(files)
+        numberCompilableFiles = 0
+        nr = 0
+        for f in files:
+            print(f'{problem}/{solution} {nr}/{numberFiles - 1}')
+
+            if(os.system(f"g++ {root}/{f} -fsyntax-only") == 0):
+                numberCompilableFiles+=1
+            nr+=1
+        
+        datasetStatistics.write(f'{problem}, {solution}, {numberFiles}, {numberCompilableFiles}\n')
+
+
+
+
