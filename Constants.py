e283b19ba9cf6a8569367ddb5d1a3e5ede947353
bfdyoy <45978395+bfdyoy@users.noreply.github.com>
2022-04-27 22:46:11 +0300

    Update Constants
    Update constants list

--- Constants.py
+++ Constants.py
@@ -1,1 +1,13 @@
 TMP_PATH = 'tmp'
+
+TFIDF_STOP_WORDS = ["(", ")", ".", "#", ";", ",", ">>", "<<", "{", "}", "[", "]", "'.'", "\"...\"", ">", "<", "std", 
+                    "fout", "fin", "++", "--", "<=", ">=", "auto", "auto", "bool","break", "case", "char", "char8_t",
+                    "char16_t", "char32_t", "class", "concept", "const", "consteval", "constexpr", "continue", 
+                    "decltype", "default", "do", "double", "else", "enum", "explicit","false", "float", "for",
+                    "if", "inline", "int", "long", "new", "noexcept", "not", "operator", "private", "protected", 
+                    "public", "return", "short", "signed", "sizeof", "static", "struct", "switch", "template", 
+                    "this", "true", "union", "unsigned", "using", "virtual", "void", "volatile", "wchar_t", "while"]
+
+TFIDF_TOKEN_PATTERN = "\w+|\+|-|=|!="
+
+NOT_COMPILABLE = 'not_compilable'
