730dd668fa3231ac2f48b711e345af73ca763798
Stoica Alenxadru <stoicaastefan@gmail.com>
2023-12-18 11:07:20 +0200

    Updated the algorithm to be more efficient

--- Analysis.py
+++ Analysis.py
@@ -1,190 +1,241 @@
-import pandas as pd
-from sklearn.metrics import accuracy_score
+# import pandas as pd
+# from sklearn.metrics import accuracy_score
 
-df = pd.read_csv('Data/Validation/NumKClusterUnsupervised.csv')
+# df = pd.read_csv('Data/Validation/NumKClusterUnsupervised.csv')
 
-df = df[df.PredNumClusters <= 4]
+# df = df[df.PredNumClusters <= 4]
 
-def precision_at_k(df:pd.DataFrame, k):
-    sorted_df = df.sort_values('Score', ascending=False)
+# def precision_at_k(df:pd.DataFrame, k):
+#     sorted_df = df.sort_values('Score', ascending=False)
 
-    true = 0
-    for idx in range(k):
-        if(sorted_df.iloc[idx]['PredNumClusters'] == sorted_df.iloc[idx]['TrueNumClusters']):
-            true+=1
+#     true = 0
+#     for idx in range(k):
+#         if(sorted_df.iloc[idx]['PredNumClusters'] == sorted_df.iloc[idx]['TrueNumClusters']):
+#             true+=1
     
-    return pd.DataFrame({f"Precision@{k}": [true / k]})
+#     return pd.DataFrame({f"Precision@{k}": [true / k]})
 
 
 
-df_at_1 = df.groupby(['Problem']).apply(lambda x: precision_at_k(x, 1))
-df_at_3 = df.groupby(['Problem']).apply(lambda x: precision_at_k(x, 3))
-df_at_5 = df.groupby(['Problem']).apply(lambda x: precision_at_k(x, 5))
-df_at_10 = df.groupby(['Problem']).apply(lambda x: precision_at_k(x, 10))
+# df_at_1 = df.groupby(['Problem']).apply(lambda x: precision_at_k(x, 1))
+# df_at_3 = df.groupby(['Problem']).apply(lambda x: precision_at_k(x, 3))
+# df_at_5 = df.groupby(['Problem']).apply(lambda x: precision_at_k(x, 5))
+# df_at_10 = df.groupby(['Problem']).apply(lambda x: precision_at_k(x, 10))
 
-df = df_at_1.merge(df_at_3, on='Problem').merge(df_at_5, on='Problem').merge(df_at_10, on='Problem')
-df.to_csv('Data/Validation/SiluetteScoreUnsupervisedClusteringK.csv')
-
-
+# df = df_at_1.merge(df_at_3, on='Problem').merge(df_at_5, on='Problem').merge(df_at_10, on='Problem')
+# df.to_csv('Data/Validation/SiluetteScoreUnsupervisedClusteringK.csv')
 
 
-# from ValidationMethods import ClusteringValidationMethod, EstimatorValidationMethod
-# from EmbeddingsLoader import Code2VecEmbeddingsLoader, InfercodeEmbeddingsLoader, W2VEmbeddingsLoader, SafeEmbeddingsLoader, TfidfEmbeddingsLoader, EmbeddingsLoader
-# from IValidationPipelines import IValidationPipelines
-# import numpy as np
-# from sklearn.cluster import KMeans, SpectralClustering, AgglomerativeClustering
-# import itertools
-# import pickle
+class Node:
+    def __init__(self, cluster):
+        self.children = []
+        self.cluster = cluster
+        self.count = 0
 
-# def _split_embeddings_data_per_problem(embeddings_loader:EmbeddingsLoader):
-#         print(f"Extracting the {embeddings_loader.get_name()} embeddings from disk")
+class MultiAssigmentClustering():
+    def __init__(self, grouped_paths, num_views, num_k):
+        self.num_views = num_views
+        self.num_k = num_k
 
-#         problem_dict = {}
+        self.found_k = 0
+        self.current_score = 0 
+        self.best_score = -1
+        self.is_used = [[0]*num_k for _ in range(num_views)]
 
-#         for solution in embeddings_loader.get_embeddings():
-#             function_embeddings = np.array(solution["embeddings"])
-#             solution_embedding = np.mean(function_embeddings, 0)
+        self.root = Node(-1)
 
-#             problem = solution['label'].split("$")[0]
-#             if(problem not in problem_dict):
-#                 problem_dict[problem] = {'indexes':[], 'X' : [], 'Y': []}
+        for path, v in grouped_paths.items():
+            self._add_to_trie(self.root, [int(x) for x in path.split('$')], num_views, len(v))
 
-#             problem_dict[problem]['indexes'].append(solution['index'])
-#             problem_dict[problem]['X'].append(solution_embedding)
-#             problem_dict[problem]['Y'].append(solution['label'])
+
+    def _add_to_trie(self, root, v, num_views, value):
+        next_node = root
+        for level in range(num_views):
+            view_cluster = v[level]
+
+            found_node = None
+            for child in next_node.children:
+                if child.cluster == view_cluster:
+                    found_node = child
+                    break
+
+            if found_node is None:
+                found_node = Node(view_cluster)
+                next_node.children.append(found_node)
+            next_node = found_node
+
+        next_node.count = value
+
+    def _fit(self, initial_root, root, level):
+        if len(root.children) == 0:
+            self.found_k += 1
+            self.current_score += root.count
+
+            if self.found_k == self.num_k:
+                if self.best_score < self.current_score:
+                    self.best_score = self.current_score
+
+                self.current_score -= root.count
+                self.found_k -= 1
+                return
+            else:
+                next_root = None
+                i = 0
+                for child in initial_root.children:
+                    if i == self.found_k:
+                        next_root = child
+                        break
+                    i += 1
+                if next_root is not None:
+                    self._fit(initial_root, next_root, 1)
+                self.current_score -= root.count
+                self.found_k -= 1
+
+        for child in root.children:
+            if not self.is_used[level][child.cluster]:
+                self.is_used[level][child.cluster] = True
+                self._fit(initial_root, child, level + 1)
+                self.is_used[level][child.cluster] = False
+
+    def fit(self):
+        self._fit(self.root, self.root.children[0], 1)
+
+
+
+
+from ValidationMethods import ClusteringValidationMethod, EstimatorValidationMethod
+from EmbeddingsLoader import Code2VecEmbeddingsLoader, InfercodeEmbeddingsLoader, W2VEmbeddingsLoader, SafeEmbeddingsLoader, TfidfEmbeddingsLoader, EmbeddingsLoader
+from IValidationPipelines import IValidationPipelines
+import numpy as np
+from sklearn.cluster import KMeans, SpectralClustering, AgglomerativeClustering
+import itertools
+import pickle
+
+def _split_embeddings_data_per_problem(embeddings_loader:EmbeddingsLoader):
+        print(f"Extracting the {embeddings_loader.get_name()} embeddings from disk")
+
+        problem_dict = {}
+
+        for solution in embeddings_loader.get_embeddings():
+            function_embeddings = np.array(solution["embeddings"])
+            solution_embedding = np.mean(function_embeddings, 0)
+
+            problem = solution['label'].split("$")[0]
+            if(problem not in problem_dict):
+                problem_dict[problem] = {'indexes':[], 'X' : [], 'Y': []}
+
+            problem_dict[problem]['indexes'].append(solution['index'])
+            problem_dict[problem]['X'].append(solution_embedding)
+            problem_dict[problem]['Y'].append(solution['label'])
         
-#         embeddings = {}
-#         embeddings['name'] = embeddings_loader.get_name()
-#         embeddings['problemDict'] = problem_dict
+        embeddings = {}
+        embeddings['name'] = embeddings_loader.get_name()
+        embeddings['problemDict'] = problem_dict
 
-#         return embeddings
-
-
-
-# class DetermineKUnsupervised(IValidationPipelines):
-#     def determine(self):
-#         clustering_algos = [KMeans(), SpectralClustering(), AgglomerativeClustering(affinity="cosine",linkage="average")]
-
-#         embeddings_list = [_split_embeddings_data_per_problem(Code2VecEmbeddingsLoader()), 
-#                         _split_embeddings_data_per_problem(SafeEmbeddingsLoader()), 
-#                         _split_embeddings_data_per_problem(TfidfEmbeddingsLoader()),
-#                         _split_embeddings_data_per_problem(InfercodeEmbeddingsLoader()),
-#                         _split_embeddings_data_per_problem(W2VEmbeddingsLoader())]
+        return embeddings
 
 
-#         embeddings_list2_combinations = itertools.combinations(embeddings_list, 2)
-#         clustering_algos2_cartesian_product = [[x,y] for x in clustering_algos for y in clustering_algos]
-#         all_hyper_parametrization2 = itertools.product(embeddings_list2_combinations,  clustering_algos2_cartesian_product)
 
-#         embeddings_list3_combinations = itertools.combinations(embeddings_list, 3)
-#         clustering_algos3_cartesian_product = [[x,y,z] for x in clustering_algos for y in clustering_algos  for z in clustering_algos]
-#         all_hyper_parametrization3 = itertools.product(embeddings_list3_combinations,  clustering_algos3_cartesian_product)
+class DetermineKUnsupervised(IValidationPipelines):
+    def determine(self):
+        clustering_algos = [KMeans(), SpectralClustering(), AgglomerativeClustering(affinity="cosine",linkage="average")]
 
-#         all_hyper_parametrization = [all_hyper_parametrization2, all_hyper_parametrization3]
-#         csv = self._create_csv_validation("NumKClusterUnsupervised")
-#         self._append_to_csv(csv, ['Problem', 'Embedding', 'Clustering', "Pred_Num_Clusters", "True_Num_Clusters", 'Score', 'Samples_size'])
-#         for hyperparametrization in all_hyper_parametrization:
-#             for cartesian_product in hyperparametrization:
-#                 embeddings_used = str.join('/', [emb['name'] for emb in cartesian_product[0]])
-#                 clustering_used = str.join('/', [type(clus).__name__ for clus in cartesian_product[1]])
-
-#                 embeddings = list(cartesian_product[0])
-#                 clustering_algos = cartesian_product[1]
-#                 problem_dicts = []
-
-#                 for i in range(0, len(embeddings)):
-#                     if(embeddings[i]['name'] == 'w2v' or embeddings[i]['name'] == 'tfidf'):
-#                         embeddings[0], embeddings[i] = embeddings[i], embeddings[0]
-#                         break
-
-#                 for embedding in embeddings:
-#                     problem_dicts.append(embedding['problemDict'])
+        embeddings_list = [_split_embeddings_data_per_problem(Code2VecEmbeddingsLoader()), 
+                        _split_embeddings_data_per_problem(SafeEmbeddingsLoader()), 
+                        _split_embeddings_data_per_problem(TfidfEmbeddingsLoader()),
+                        _split_embeddings_data_per_problem(InfercodeEmbeddingsLoader()),
+                        _split_embeddings_data_per_problem(W2VEmbeddingsLoader())]
 
 
-#                 for problem , data in problem_dicts[0].items():
-#                     for num_clusters in range(2,5):
-#                         common_clusters_per_view = []
-#                         clusters_n = []
+        embeddings_list2_combinations = itertools.combinations(embeddings_list, 2)
+        clustering_algos2_cartesian_product = [[x,y] for x in clustering_algos for y in clustering_algos]
+        all_hyper_parametrization2 = itertools.product(embeddings_list2_combinations,  clustering_algos2_cartesian_product)
+
+        embeddings_list3_combinations = itertools.combinations(embeddings_list, 3)
+        clustering_algos3_cartesian_product = [[x,y,z] for x in clustering_algos for y in clustering_algos  for z in clustering_algos]
+        all_hyper_parametrization3 = itertools.product(embeddings_list3_combinations,  clustering_algos3_cartesian_product)
+
+        all_hyper_parametrization = [all_hyper_parametrization2, all_hyper_parametrization3]
+        csv = self._create_csv_validation("NumKClusterUnsupervised")
+        self._append_to_csv(csv, ['Problem', 'Embedding', 'Clustering', "Pred_Num_Clusters", "True_Num_Clusters", 'Score', 'Samples_size'])
+        for hyperparametrization in all_hyper_parametrization:
+            for cartesian_product in hyperparametrization:
+                embeddings_used = str.join('/', [emb['name'] for emb in cartesian_product[0]])
+                clustering_used = str.join('/', [type(clus).__name__ for clus in cartesian_product[1]])
+
+                embeddings = list(cartesian_product[0])
+                clustering_algos = cartesian_product[1]
+                problem_dicts = []
+
+                for i in range(0, len(embeddings)):
+                    if(embeddings[i]['name'] == 'w2v' or embeddings[i]['name'] == 'tfidf'):
+                        embeddings[0], embeddings[i] = embeddings[i], embeddings[0]
+                        break
+
+                for embedding in embeddings:
+                    problem_dicts.append(embedding['problemDict'])
+
+
+                for problem , data in problem_dicts[0].items():
+                    for num_clusters in range(2,11):
+                        common_clusters_per_view = []
+                        clusters_n = []
 
                         
-#                         for i in range(len(embeddings)):
-#                             common_clusters_per_view.append([])
-#                             with open(f"Data/Clusterings/{type(clustering_algos[i]).__name__}_{embeddings[i]['name']}_{problem}_{num_clusters}_labels.bin", 'rb') as fp:
-#                                 clusters_n.append(pickle.load(fp))
+                        for i in range(len(embeddings)):
+                            common_clusters_per_view.append([])
+                            with open(f"Data/Clusterings/{type(clustering_algos[i]).__name__}_{embeddings[i]['name']}_{problem}_{num_clusters}_labels.bin", 'rb') as fp:
+                                clusters_n.append(pickle.load(fp))
 
-#                         Y = []
-#                         for index in data['indexes']:
-#                                 index_in_all_embeddings = True
+                        Y = []
+                        for index in data['indexes']:
+                                index_in_all_embeddings = True
 
-#                                 for problem_dict in problem_dicts:
-#                                     if index not in problem_dict[problem]['indexes']:
-#                                         index_in_all_embeddings = False
-#                                         break
+                                for problem_dict in problem_dicts:
+                                    if index not in problem_dict[problem]['indexes']:
+                                        index_in_all_embeddings = False
+                                        break
 
-#                                 if(index_in_all_embeddings):
-#                                     i = 0
-#                                     for problem_dict in problem_dicts:
-#                                         problem_data = problem_dict[problem]
-#                                         common_clusters_per_view[i].append(clusters_n[i][problem_data['indexes'].index(index)])
-#                                         i+=1
-#                                     Y.append(data['Y'][data['indexes'].index(index)])
+                                if(index_in_all_embeddings):
+                                    i = 0
+                                    for problem_dict in problem_dicts:
+                                        problem_data = problem_dict[problem]
+                                        common_clusters_per_view[i].append(clusters_n[i][problem_data['indexes'].index(index)])
+                                        i+=1
+                                    Y.append(data['Y'][data['indexes'].index(index)])
 
-#                         all_labels = list(set(Y))
+                        all_labels = list(set(Y))
 
-#                         def check_distinct_combination(combination):
-#                             for x in combination:
-#                                 for y in combination:
-#                                     if x != y:
-#                                         clustersX = x.split("$")
-#                                         clustersY = y.split("$")
-
-#                                         for i in range(len(clustersX)):
-#                                             if(clustersX[i] == clustersY[i]):
-#                                                 return False
-#                             return True
                                     
-#                         label_n = np.array(common_clusters_per_view).T
+                        label_n = np.array(common_clusters_per_view).T
 
-#                         grouped_paths = {}
+                        grouped_paths = {}
                         
-#                         i = 0
-#                         for path in label_n:
-#                             key = ""
+                        i = 0
+                        for path in label_n:
+                            key = ""
 
-#                             for x in path:
-#                                 key+=str(x) + '$'
-#                             key = key[:-1]
+                            for x in path:
+                                key+=str(x) + '$'
+                            key = key[:-1]
 
-#                             if(key in grouped_paths):
-#                                 grouped_paths[key].append(i)
-#                             else:
-#                                 grouped_paths[key] = [i]
-#                             i+=1
+                            if(key in grouped_paths):
+                                grouped_paths[key].append(i)
+                            else:
+                                grouped_paths[key] = [i]
+                            i+=1
                 
-#                         best_score = - 1
-#                         best_combination = []
-#                         grouped_paths_keys = grouped_paths.keys()
+                        mac = MultiAssigmentClustering(grouped_paths, len(cartesian_product[0]), num_clusters)
+                        mac.fit()
+                        best_score = mac.best_score
+                        print(f"Executing on problem {problem} with {num_clusters} clusters, embeddings {embeddings_used} and clustering methods {clustering_used}: Best score {best_score}")
 
-#                         for combination in itertools.combinations(grouped_paths_keys, num_clusters):
-#                             if(check_distinct_combination(combination)):
-#                                 score = 0
-
-#                                 for key in combination:
-#                                     score += len(grouped_paths[key])
-                                
-#                                 if(score > best_score):
-#                                     best_score = score
-#                                     best_combination = combination
-
-#                         best_score = best_score
-#                         print(f"Executing on problem {problem} with {num_clusters} clusters, embeddings {embeddings_used} and clustering methods {clustering_used}: Best score {best_score}")
-
-#                         self._append_to_csv(csv, [problem, embeddings_used, clustering_used, str(num_clusters), str(len(all_labels)), str(best_score), str(label_n.shape[0])])
+                        self._append_to_csv(csv, [problem, embeddings_used, clustering_used, str(num_clusters), str(len(all_labels)), str(best_score), str(label_n.shape[0])])
 
 
-# d = DetermineKUnsupervised()
+d = DetermineKUnsupervised()
 
-# d.determine()
+d.determine()
 
 # import pandas as pd
 # from sklearn.metrics import accuracy_score
